5cc6f38852ae77b17a2f06d097d18880825e4e66
ytyaru <ytyaru@outlook.jp>
2019-05-31 05:12:07 +0900

    初回コミット

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * Rustのデータ型（論理値）
+ * CreatedAt: 2019-05-31
+ */
+fn main() {
+    let is_a = true;
+    let is_b: bool = false;
+    println!("{} {}", is_a, is_b);
+    if is_a { println!("true!"); }
+}
+
